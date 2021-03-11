6d8918e80fa6581c0c81dc6bc085e861aa668efc
mairagee524 <garciamaira524@yahoo.com>
2021-03-10 23:13:44 -0800

    made changes to navbar formatting

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -2,18 +2,22 @@ import { extendTheme } from "@chakra-ui/react";
 
 const colors = {
   primary: {
-    100: "#E5FCF1",
-    200: "#27EF96",
-    300: "#10DE82",
-    400: "#0EBE6F",
-    500: "#0CA25F",
-    600: "#0A864F",
-    700: "#086F42",
-    800: "#075C37",
-    900: "#064C2E"
+    100: "#FCDC84",
+    200: "#7C4C74",
+    300: "#642404",
+    400: "#FV5C14",
+    500: "#EC4494",
+    600: "#2395CB",
   }
 };
 
-const customTheme = extendTheme({ colors });
+const breakpoints = createBreakpoints({
+  sm: "320px",
+  md: "768px",
+  lg: "960px",
+  xl: "1200px",
+})
+
+const customTheme = extendTheme({ colors, breakpoints });
 
 export default customTheme;
